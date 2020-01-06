a710ea5463cacade4a1390446bbe797da8cb7b85
Akbarsn <new.akbarsatya@gmail.com>
2020-01-06 18:22:23 +0700

    Creating route for login and testing it

---
+++ auth/users.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = express.Router();
+const model = require("../models/index");
+
+router.post("/login", async (req, res) => {
+  try {
+    const user = await model.users.findOne({
+      where: { username: req.body.username }
+    });
+    if (user) {
+      if (user.password === req.body.password) {
+        res.json({
+          status: "Login"
+        });
+      } else {
+        res.json({
+          status: "Password Salah"
+        });
+      }
+    } else {
+      res.json({
+        status: "User tidak ditemukan"
+      });
+    }
+  } catch (err) {
+    res.json({
+      status: "Error"
+    });
+  }
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const users = require("./auth/users");
 
 app.use(express.json());
+app.use("/auth", users);
 
 app.listen(port, () => {
     console.log("Listening to " + port);
