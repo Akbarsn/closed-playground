a5213057197c70ef8d81284dc6c65a5a19b62b3f
Akbarsn <new.akbarsatya@gmail.com>
2020-01-12 18:56:44 +0700

    feat: Add deleteNote route

--- route/notes.js
+++ route/notes.js
@@ -109,4 +109,22 @@ router.put("/:id", async (req, res) => {
   }
 });
 
+router.delete("/:id", (req, res) => {
+  model.notes
+    .destroy({ where: { id: req.params.id } })
+    .then(result => {
+      res.status(200).json({
+        status: "Deleted",
+        message: "Notes deleted",
+        data: result
+      });
+    })
+    .catch(err => {
+      res.status(400).json({
+        status: "Error",
+        message: "Error when deleting"
+      });
+    });
+});
+
 module.exports = router;
